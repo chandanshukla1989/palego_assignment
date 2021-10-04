dc06b59923f94c6528334d1c3679d6fe4ceab73e
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2021-10-05 00:52:04 +0530

    Update reddit_account.py

--- reddit_api/reddit_account.py
+++ reddit_api/reddit_account.py
@@ -1,1 +1,9 @@
+import praw
 
+reddit = praw.Reddit(
+    client_id="CCXVctIyT5n9JHNStRYdHg",
+    client_secret="gW3wfOTtlLVjnjAhgq9vtN7dCRiE0w",
+    password="m&345Ch0@123",
+    user_agent="Palego Assignment by u/Chandan",
+    username="chandan1987",
+)
