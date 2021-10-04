6e2bcaf444c18275abfe15b9ae8fd0c9e988083b
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2021-10-05 01:06:24 +0530

    Update base.py

--- kafka/config/base.py
+++ kafka/config/base.py
@@ -1,2 +1,2 @@
 kafka_local_topic='AWSKafkaTutorialTopic'
-kafka_broker_list=
+kafka_broker_list="['b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.com:9092', 'b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.com:9092']"
