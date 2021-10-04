f10b5fc1386d7528f34781fe3560b40577f1cac3
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2021-10-05 01:08:33 +0530

    Update consumer.py

--- kafka/consumer/consumer.py
+++ kafka/consumer/consumer.py
@@ -43,7 +43,8 @@ def consume_msg_s3(consumer):
         print('This is just to show example that we can create multiple consumer groups and consume message in many destinations like Dynamo, RDS, S3, Redshift for simplicity only dynamo db consumer has been covered  :'+'{}'.format(id))
 if __name__ == "__main__":
 
-    brokers=['b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.com:9092', 'b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.c                                                                                  om:9092']
+    #brokers=['b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.com:9092', 'b-1.demo-cluster-1.ww4d4r.c7.kafka.us-east-2.amazonaws.com:9092']
+    brokers=kafka_broker_list
 # Define Kafka topic to be produced to
     kafka_topic=kafka_local_topic
 
